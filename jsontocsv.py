21f04c1f7fdc45e1ad56afe84194c24a038b6320
zhaijiahui <826806614@qq.com>
2018-08-29 23:41:40 +0800

    初始提交

---
+++ jsontocsv.py
@@ -0,0 +1,53 @@
+#-*-coding:utf-8-*-
+import csv
+import json
+import sys
+import codecs
+from collections import OrderedDict
+
+
+def list_dictionary(dic,title,value,key=''):
+    flag = 0
+    for k,v in dic.items():
+        if type(dic[k]) is OrderedDict: # keep sequence
+            temp = dic[k]
+            key = k
+            flag = 1
+            list_dictionary(temp,title,value,key)
+        else:
+            if flag:
+                title.append(k)
+            else:
+                title.append(key+"_"+k)
+            value.append(dic[k])
+
+def trans(path):
+    jsonData = codecs.open(path, 'r', 'utf-8')
+    # csvfile = open(path+'.csv', 'wb') # python2
+    csvfile = open(path+'.csv', 'w', newline='') # python3
+    writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+    value_list = []
+    for line in jsonData:
+        dic = json.loads(line,object_pairs_hook=OrderedDict)
+        title = []
+        value = []
+        list_dictionary(dic,title,value)
+        # print(value)
+        value_list.append(value)
+        
+    writer.writerow(title)
+    for i in value_list:
+        try:
+            writer.writerow(i)
+        except Exception as e: # UnicodeEncodeError
+            pass
+
+    jsonData.close()
+    csvfile.close()
+
+if __name__ == '__main__':
+    path=str(sys.argv[1]) # 获取path参数
+    # print(path)
+    trans(path)
+    print('Complete! save to:'+path+'.csv')
+    # trans('target.json')
